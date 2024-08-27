2905af0581069b2c1676001a167e78744fc518e2
Monoswita Shome <137887898+Monoswita-94@users.noreply.github.com>
2024-08-27 21:36:09 +0600

    Update Add two numbers.asm

--- Assembly Language Programming/Add two numbers.asm
+++ Assembly Language Programming/Add two numbers.asm
@@ -1,13 +1,46 @@
+.model small 
+.stack 100h
+.data
+a db 10,13, 'Input first digit $'
+b db 10,13, 'Input second digit $'
+c db 10,13, 'Result $'
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
+.code
+main proc
+mov ax,@data
+mov ds,ax
 
-org 100h
+mov ah,9 ;show 1st msg
+lea dx,a
+int 21h
 
-; add your code here
+mov ah,1  ;1st input value
+int 21h
+mov bl,al
 
-ret
+mov ah,9;show 2nd msg
+lea dx,b
+int 21h
+
+mov ah,1
+int 21h
+mov bh,al
+
+mov ah,9   ;show 3rd msg
+lea dx,c
+int 21h 
+
+add bl,bh ;  bl = bl + bh
+sub bl,48 ;bl=bl-48
+           
+
+mov ah,2
+mov dl,bl
+int 21h
 
 
-
-
+exit: 
+mov ah,4ch
+int 21h
+main endp
+end main
